57603bbf68cb79f45078b6f03ce00eac6afe03bd
Filip Osiński <filip.osinski@devoteam.com>
2022-04-12 21:29:47 +0200

    Removed unused variable

--- src/lwc/imageCropper/imageCropper.js
+++ src/lwc/imageCropper/imageCropper.js
@@ -15,7 +15,6 @@ export default class ImageCropper extends LightningElement {
     imageMimeType = '';
     imageName = '';
 
-    croppedImageBlob;
     croppedImageObject = {};
 
     @api aspectRatio = null;
